2be2bcb7d086457b488946aad5e7147e92d426ec
William Santos <william.santos88@gmail.com>
2019-07-27 23:03:14 -0300

    Incluindo lógica while

--- advinhacao.py
+++ advinhacao.py
@@ -3,19 +3,24 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
-chute = int(input("Digite o seu número: "))
-print("Você digitou ", chute)
+total_de_tentativas = 3
+rodada = 1
 
-acertou = chute == numero_secreto
-menor = chute < numero_secreto
-maior = chute > numero_secreto
+while(rodada <= total_de_tentativas):
 
-if (acertou):
-    print("Parabéns, você acertou!")
-else:
-    if(menor):
-        print("O número informado é menor do que o número secreto.")
-    elif(maior):
-        print("O número informado é maior do que o número secreto.")
+    chute = int(input("Digite o seu número: "))
+    print("Você digitou ", chute)
 
+    acertou = chute == numero_secreto
+    menor = chute < numero_secreto
+    maior = chute > numero_secreto
+
+    if (acertou):
+        print("Parabéns, você acertou!")
+    else:
+        if(menor):
+            print("O número informado é menor do que o número secreto.")
+        elif(maior):
+            print("O número informado é maior do que o número secreto.")
+    rodada = rodada + 1
 print("Fim do Jogo!!")
