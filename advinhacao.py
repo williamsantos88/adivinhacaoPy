2edd4ca3ca65eff3230742072ea2c217736cb293
William Santos <william.santos88@gmail.com>
2019-07-27 22:48:30 -0300

    Incluindo lógica if, else e elif

--- advinhacao.py
+++ advinhacao.py
@@ -3,12 +3,19 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
-chute = input("Digite o seu número: ")
+chute = int(input("Digite o seu número: "))
 print("Você digitou ", chute)
 
-if numero_secreto == int(chute):
+acertou = chute == numero_secreto
+menor = chute < numero_secreto
+maior = chute > numero_secreto
+
+if (acertou):
     print("Parabéns, você acertou!")
 else:
-    print("Você errou...")
+    if(menor):
+        print("O número informado é menor do que o número secreto.")
+    elif(maior):
+        print("O número informado é maior do que o número secreto.")
 
 print("Fim do Jogo!!")
