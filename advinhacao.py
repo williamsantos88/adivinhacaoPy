9bd38a7a9ba73f03dc2a4815ef786df981ea01b2
williamsantos88 <46947008+williamsantos88@users.noreply.github.com>
2021-01-29 00:33:03 -0300

    Incluindo nível de dificuldade

--- advinhacao.py
+++ advinhacao.py
@@ -7,6 +7,18 @@ print("*********************************")
 numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
 
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil)")
+
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+        total_de_tentativas = 20
+elif(nivel == 2):
+        total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
